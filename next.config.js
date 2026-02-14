8c622b3fb98f7053b37f900f01469ce248a81b45
Jitendra Singh <i.jitendra.singh0@gmail.com>
2026-02-14 21:07:01 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   // Performance: Use SWC for faster minification
 
   // Images: Prioritize modern formats for Apple-like crispness
